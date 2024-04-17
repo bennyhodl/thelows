bf94aa762c4b2755d3924db9e3600c801a421d9a
bennyhodl <ben@bitcoinbay.live>
2024-04-17 15:56:17 -0400

    Leaderboard server component

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,40 +1,27 @@
 "use client"
 import { Footer } from "@/components/Footer";
-import { Suspense, useEffect, useState } from "react";
-import axios from "axios";
+import { Suspense } from "react";
 import { Header } from "@/components/Header";
 import { API_URL } from "@/lib/utils";
 import { LeaderBoardResponse, LeaderboardSong, theLows } from "@/lib/types";
-import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Home() {
-  const [loading, setLoading] = useState<boolean>(false)
-  const [lb, setLb] = useState<LeaderBoardResponse>({ total: 0, songs: [] })
-  const getLeaderboard = async () => {
-    setLoading(true)
-    const leaderboard = await axios.get<LeaderBoardResponse>(`${API_URL}/api/leaderboard`)
-    const sorted = leaderboard.data.songs.sort((a, b) => Number(b.percent) - Number(a.percent))
-    setLb({ total: leaderboard.data.total, songs: sorted })
-    setLoading(false)
-  }
-  useEffect(() => {
-    getLeaderboard()
-  }, [])
+const getLeaderboard = async (): Promise<LeaderBoardResponse> => {
+  const response = await fetch(`${API_URL}/api/leaderboard`)
+  const leaderboard: LeaderBoardResponse = await response.json()
+  leaderboard.songs.sort((a, b) => Number(b.percent) - Number(a.percent))
+  return leaderboard
+}
+
+export default async function Leaderboard() {
+  const leaderboard = await getLeaderboard()
+
   return (
-    <Suspense>
+    <Suspense fallback={<p className="text-red text-3xl bg-blue-400">heyhowareya</p>}>
       <div className="flex flex-col justify-between items-center h-screen bg-gray-950 md:max-w-lg m-auto font-bold">
         <Header />
         <div className="w-full flex justify-center flex-col items-center text-white">
           <h1 className="pt-14 pb-6 text-3xl text-center">Leaderboard</h1>
-          {loading && (
-            theLows.map(song => (
-              <div key={song}>
-                <Skeleton className="h-5 w-80 rounded-md mb-1" />
-                <Skeleton className="h-8 w-80 rounded-md mb-2" />
-              </div>
-            ))
-          )}
-          {lb.songs && !loading && lb.songs.map(song => <SongBar key={song.name} song={song} />)}
+          {leaderboard.songs.map(song => <SongBar key={song.name} song={song} />)}
         </div>
         <Footer />
       </div >
